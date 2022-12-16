1b5a106d2c5b81e196c4930f6fadbb3156feaaf8
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-12-16 15:09:58 +0100

    v1 of createRecommendationsList

--- src/extension/createRecommendationsList.ts
+++ src/extension/createRecommendationsList.ts
@@ -9,26 +9,39 @@ export type RecommendationsListCreator = (cfg: ExperimentConfig) =>
 (nonPersonalized: Recommendation[], personalized: Recommendation[]) =>
 Recommendation[];
 
+// Fisher-Yates shuffle (https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
+const shuffleArray = <T>(array: T[]): T[] => {
+	const shuffledArray = [...array];
+	for (let i = shuffledArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+	}
+
+	return shuffledArray;
+};
+
+const removeDuplicates = <T>(array: T[]): T[] => [...new Set(array)] as T[];
+
 export const createRecommendationsList: RecommendationsListCreator = cfg =>
 	(nonPersonalized, personalized) => {
 		const limit = Math.min(personalized.length, nonPersonalized.length);
 
-		const result: Recommendation[] = [];
+		const tmpResult: Recommendation[] = [];
 		const unused: Recommendation[] = [];
 
 		for (let i = 0; i < limit; i++) {
 			const takeNonPersonalized = Math.random() < cfg.nonPersonalizedProbability;
 
 			if (takeNonPersonalized) {
-				result.push(nonPersonalized[i]);
+				tmpResult.push(nonPersonalized[i]);
 				unused.push(personalized[i]);
 			} else {
-				result.push(personalized[i]);
+				tmpResult.push(personalized[i]);
 				unused.push(nonPersonalized[i]);
 			}
 		}
 
-		return result.concat(unused);
+		return removeDuplicates(tmpResult.concat(shuffleArray(unused)));
 	};
 
 export default createRecommendationsList;
