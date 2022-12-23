bc2c22627206fea05c3ce9ade1afd3d0f7c2ded4
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-12-23 11:34:21 +0100

    add react router

--- src/server/app/index.tsx
+++ src/server/app/index.tsx
@@ -3,8 +3,20 @@ import {createRoot} from 'react-dom/client';
 
 import {ThemeProvider} from '@mui/material';
 
+import {
+	createBrowserRouter as createRouter,
+	RouterProvider,
+} from 'react-router-dom';
+
 import theme from './theme';
 import Server from './Server';
+
+const router = createRouter([
+	{
+		path: '/',
+		element: <Server />,
+	}
+]);
 
 const elt = document.getElementById('app');
 
@@ -12,4 +24,10 @@ if (!elt) {
 	throw new Error('No element with id "app" found');
 }
 
-createRoot(elt).render(<ThemeProvider theme={theme}><Server /></ThemeProvider>);
+createRoot(elt).render((
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	</React.StrictMode>
+));
