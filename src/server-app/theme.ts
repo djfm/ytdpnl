27362b6153f66c85a8107f082df9b00c252bb0a6
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-01-01 09:01:43 +0100

    change h1 font size

--- src/server-app/theme.ts
+++ src/server-app/theme.ts
@@ -2,4 +2,11 @@ import {createTheme} from '@mui/material';
 
 export const theme = createTheme({});
 
+theme.typography.h1 = {
+	fontSize: '1.5rem',
+	[theme.breakpoints.up('sm')]: {
+		fontSize: '2.5rem',
+	},
+};
+
 export default theme;
