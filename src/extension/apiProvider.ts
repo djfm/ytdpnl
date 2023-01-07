c5f2a069646dfb20d3222c0b2ced48bb435e4206
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-01-07 16:23:07 +0100

    ext prod build

--- src/extension/apiProvider.ts
+++ src/extension/apiProvider.ts
@@ -6,12 +6,14 @@ import {has} from '../util';
 import config from '../../config.extension';
 
 const env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 if (!has(`${env}-server-url`)(config)) {
 	throw new Error(`Missing ${env}-server-url in config.extension.ts`);
 }
 
 const serverUrl = config[`${env}-server-url`];
+console.log('API URL:', serverUrl);
 
 export const defaultApi = createApi(serverUrl);
 
