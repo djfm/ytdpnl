d388ca1c38a19a8353ece4293002fa43997dcbf2
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-01-12 09:47:33 +0100

    fixed firefox permissions

--- webpack.config.extension.js
+++ webpack.config.extension.js
@@ -7,10 +7,10 @@ const mode = process.env.NODE_ENV || 'development';
 const isProduction = mode === 'production';
 
 const hostPermissions = [
-	'https://*.youtube.com/*',
-	'https://*.youtu.be/*',
-	'https://ytdpnl-dev.fmdj.fr/api/*',
-	'https://ytdnlp.fmdj.fr/api/*',
+	'*://*.youtube.com/*',
+	'*://*.youtu.be/*',
+	'*://ytdpnl-dev.fmdj.fr/*',
+	'*://ytdpnl.fmdj.fr/*',
 ];
 
 module.exports = {
@@ -60,7 +60,7 @@ module.exports = {
 						// eslint-disable-next-line camelcase
 						manifest.manifest_version = 3;
 						// eslint-disable-next-line camelcase
-						manifest.host_permissions = hostPermissions;
+						manifest.host_permissions = ['webRequest', ...hostPermissions];
 						return JSON.stringify(manifest, null, '\t');
 					},
 				},
